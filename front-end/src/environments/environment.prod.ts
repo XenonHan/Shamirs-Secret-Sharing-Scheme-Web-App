fb94273a9314c28d801967de5ab4657dcaf0dda8
Xenon <s10110040@gmail.com>
2021-02-10 06:52:07 +0800

    tiny update

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/CD1/fyp'
+  // apiUrl: 'http://localhost:8080/CD1/fyp'
+  apiUrl: 'http://18.162.209.227:8080/CD1/fyp'
 };
