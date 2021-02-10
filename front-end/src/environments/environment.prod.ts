d5c0e13ea4e65588e58078d8d4e29437374a16c9
Xenon <s10110040@gmail.com>
2021-02-11 06:36:06 +0800

    update ssl

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://localhost:8080/CD1/fyp'
-  apiUrl: 'http://18.162.209.227:8080/CD1/fyp'
+  //apiUrl: 'http://18.162.209.227:8080/CD1/fyp'
+  apiUrl: 'https://ssssonline.tk:8443/CD1/fyp'
 };
